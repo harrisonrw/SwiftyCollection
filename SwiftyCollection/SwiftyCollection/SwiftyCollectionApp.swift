b99942ea01f945605c1857e02897082492419504
Robert Harrison <harrisonrw@gmail.com>
2021-01-06 10:38:17 -0800

    Initial Xcode Project

---
+++ SwiftyCollection/SwiftyCollection/SwiftyCollectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyCollectionApp.swift
+//  SwiftyCollection
+//
+//  Created by Robert Harrison on 1/6/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyCollectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
