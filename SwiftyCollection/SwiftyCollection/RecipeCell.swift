6270ff34ec3ccc625bc70ab7982dbbc7ca452401
Robert Harrison <harrisonrw@gmail.com>
2021-01-08 11:44:16 -0800

    Toggle Favorite Recipes

--- SwiftyCollection/SwiftyCollection/RecipeCell.swift
+++ SwiftyCollection/SwiftyCollection/RecipeCell.swift
@@ -9,25 +9,51 @@ import SwiftUI
 
 struct RecipeCell: View {
     
-    var recipe: Recipe
+    @State var recipe: Recipe
     
     var body: some View {
         
-        VStack {
-            Image(recipe.image)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .cornerRadius(10)
-                .clipped()
+        ZStack {
+            VStack {
+                Image(recipe.image)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+                    .clipped()
+                
+                Text(recipe.title)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.black)
+                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                      
+                Spacer()
+            }
             
-            Text(recipe.title)
-                .multilineTextAlignment(.center)
-                .foregroundColor(.black)
-                .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
-                  
-            Spacer()
+            VStack(alignment: .leading) {
+                HStack(alignment: .top) {
+                    Spacer()
+                    
+                    Button(action: {
+                        self.recipe.isFavorite = !self.recipe.isFavorite
+                        
+                    }) {
+                       
+                        let color = self.recipe.isFavorite ? Color(red: 248.0/255.0, green: 55.0/255.0, blue: 32.0/255.0) : Color.white
+                        
+                            Image(systemName: "heart.fill")
+                                .font(.system(size: 12, weight: .semibold))
+                                .frame(width: 32, height: 32)
+                                .foregroundColor(color)
+                                
+                    }
+                    .background(Color(red: 0.0, green: 0.0, blue: 0.0, opacity: 0.65))
+                    .cornerRadius(16)
+                    .padding(EdgeInsets(top: 10, leading: 0, bottom: 0, trailing: 10))
+                }
+                
+                Spacer()
+            }
         }
-        
     }
 }
 
